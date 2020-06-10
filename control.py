78a7a387d8cf09b6f71d31970bfa410c5ac67f54
ana <tanase.anamaria97@gmail.com>
2020-06-10 01:03:08 +0100

    Updated control script

--- control.py
+++ control.py
@@ -11,17 +11,28 @@ port = 1883
 sleep_time = 10
 in1 = 24
 in2 = 23
-en = 25
+in3 = 17
+in4 = 27
+ena = 25
+enb = 26
 temp1=1
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+GPIO.setup(ena,GPIO.OUT)
+GPIO.setup(enb,GPIO.OUT)
+
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
-p=GPIO.PWM(en,1000)
-p.start(25)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
+duty_cicle_actuator=GPIO.PWM(ena,1000)
+duty_cicle_sprinkler=GPIO.PWM(enb,1000)
+duty_cicle_actuator.start(25)
+duty_cicle_sprinkler.start(26)
 
 def on_connect(client, userdata, flags, rc):
     if rc==0:
@@ -38,17 +49,22 @@ def on_disconnect(client, userdata, rc):
     client.disconnect_flag=True
 
 def on_message(client, data, msg):
-    print(msg.topic + " " + str(msg.qos))
+    print("TESTING SUBSCRIBE" + msg.topic + " " + str(msg.qos))
     print(json.dumps(json.loads(msg.payload.decode()), indent=4, sort_keys=True))
     payload = json.loads(msg.payload.decode())
     if (payload["general"]["temperature"] > 25):
-        control_actuator('b')
+        #control_actuator('open')
         print("caz deschide")
     elif (payload["general"]["temperature"] < 10):
-        control_actuator('f')
+#        control_actuator('close')
         print("caz inchide")
+    elif (payload["poisonous"]["smoke"] >15):
+#        control_sprinkler("water")
+        print("uda")
+    elif (payload["poisonous"]["smoke"] <15):
+#        control_sprinkler("stop")
+        print("nimic")
 
- 
 def on_publish(client, data, mid):
     print("mid: "+ str(mid))
 
@@ -97,13 +113,41 @@ def main():
     while True:
         time.sleep(sleep_time)
         pass
+        
+def control_sprinkler(action):
+    if action =='water':
+        GPIO.output(in3,GPIO.HIGH)
+        GPIO.output(in4,GPIO.LOW)
+        print("Sprinkler is now watering the area")
+        action = 'z'
+    elif action =='stop':
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.LOW)
+        print("Sprinkler has stopped watering the area")
+        action = 'z'
+    elif action == "fast":
+        duty_cicle_sprinkler.ChangeDutyCycle(75)
+        print("Sprinkler's duty cicle is now FAST")
+        action = 'z'
+    elif action == "medium":
+        duty_cicle_sprinkler.ChangeDutyCycle(50)
+        print("Sprinkler's duty cicle is now MEDIUM")
+        action = 'z'
+    elif action == "low":
+        duty_cicle_sprinkler.ChangeDutyCycle(25)
+        print("Sprinkler's duty cicle is now LOW")
+        action = 'z'
+    else:
+        GPIO.cleanup()
+        print("Unfortunately, an error has occured")
+           
 def control_actuator(action):
     if action=='r':
         print("run")
         if(temp1==1):
          GPIO.output(in1,GPIO.HIGH)
          GPIO.output(in2,GPIO.LOW)
-         print("forward")
+         print("close")
          action='z'
         else:
          GPIO.output(in1,GPIO.LOW)
@@ -112,51 +156,51 @@ def control_actuator(action):
          action='z'
 
 
-    elif action=='s':
+    elif action=='stop':
         print("stop")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.LOW)
         action='z'
 
-    elif action=='f':
-        print("forward")
+    elif action=='close':
+        print("Closing window")
         GPIO.output(in1,GPIO.HIGH)
         GPIO.output(in2,GPIO.LOW)
         temp1=1
         action='z'
 
-    elif action=='b':
-        print("backward")
+    elif action=='open':
+        print("Opening window")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.HIGH)
         temp1=0
         action='z'
 
-    elif action=='l':
-        print("low")
-        p.ChangeDutyCycle(25)
+    elif action=='low':
+        print("Actuator's duty cicle is LOW")
+        duty_cicle_actuator.ChangeDutyCycle(25)
         action='z'
 
-    elif action=='m':
-        print("medium")
-        p.ChangeDutyCycle(50)
+    elif action=='medium':
+        print("Actuator's duty cicle is MEDIUM")
+        duty_cicle_actuator.ChangeDutyCycle(50)
         action='z'
 
-    elif action=='h':
-        print("high")
-        p.ChangeDutyCycle(75)
+    elif action=='high':
+        print("Actuator's duty cicle is HIGH")
+        duty_cicle_actuator.ChangeDutyCycle(75)
         action='z'
-     
-    
-    elif action=='e':
-        GPIO.cleanup()
     
     else:
-        print("<<<  wrong data  >>>")
-        print("please enter the defined data to continue.....")
+        GPIO.cleanup()
+        print("Unfortunately, an error has occured")
+        
 def subscribe_to_topics(client):
     owners_topic = "users/"
     devices_topic = "devices/"
+
+    username = os.environ["USERNAME"]
+    client.subscribe(devices_topic + username, 0)
 
     owner = os.environ["OWNER"]
     sense_devices = os.environ["SENSE"].split(" ")
@@ -168,4 +212,7 @@ def subscribe_to_topics(client):
 
 if __name__ == "__main__":
     main()
+
+
+
 
