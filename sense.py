c8a1d20c92f34ae9ab31303deabb5302868db575
Mahmoud <hamdan.mahmoudtudor@gmail.com>
2020-06-17 15:58:15 +0300

    Small tweaks for lisibility

--- sense.py
+++ sense.py
@@ -29,10 +29,9 @@ GAS_CH4 = 0
 GAS_CO = 1
 GAS_SMOKE = 2
 
-# LPGCurve  =  [2.3,0.21,-0.47]
 CH4Curve = [2.3, 0.47, -0.36]
-COCurve = [2.3,0.72,-0.34]
-SmokeCurve = [2.3,0.53,-0.44]
+COCurve = [2.3, 0.72, -0.34]
+SmokeCurve = [2.3, 0.53, -0.44]
 
 Ro = 10
 
@@ -41,6 +40,7 @@ DIGITAL_OUTPUT_PIN = 16
 PWM_PIN = 12
 UART_PIN = 14
 ANALOG_PIN = 0
+
 
 class GroveGasSensorMQ2:
     def __init__(self, channel):
@@ -52,8 +52,10 @@ class GroveGasSensorMQ2:
         value = self.adc.read_raw(self.channel)
         return value
 
+
 def MQResistanceCalculation(raw_adc):
     return RL_VALUE*(4095-raw_adc)/raw_adc
+
 
 def MQGetGasPercentage(rs_ro_ratio, gas_id):
     if (gas_id == GAS_CH4):
@@ -65,30 +67,27 @@ def MQGetGasPercentage(rs_ro_ratio, gas_id):
 
     return 0
 
+
 def MQGetPercentage(rs_ro_ratio, pcurve):
-    return pow(10, (((math.log(rs_ro_ratio,10)-pcurve[1])/pcurve[2])+pcurve[0]))
+    return pow(10, (((math.log(rs_ro_ratio, 10)-pcurve[1])/pcurve[2])+pcurve[0]))
+
 
 class GroveBME680Sensor(object):
     def __init__(self):
         snr = bm.BME680(bm.I2C_ADDR_PRIMARY)
 
-        # over sample, make data more accuracy
+        # over sampling
         snr.set_humidity_oversample(bm.OS_2X)
         snr.set_pressure_oversample(bm.OS_4X)
         snr.set_temperature_oversample(bm.OS_8X)
         snr.set_filter(bm.FILTER_SIZE_3)
         snr.set_gas_status(bm.ENABLE_GAS_MEAS)
 
-        # profile # 0
         snr.set_gas_heater_temperature(320)
         snr.set_gas_heater_duration(150)
         snr.select_gas_heater_profile(0)
 
-        # profile # 1
-        # snr.set_gas_heater_profile(200, 150, nb_profile=1)
-        # snr.select_gas_heater_profile(1)
-
-        # Initial reading
+        # Initial readings
         snr.get_sensor_data()
         snr.get_sensor_data()
         snr.get_sensor_data()
@@ -100,32 +99,42 @@ class GroveBME680Sensor(object):
             return self.snr.data
         return None
 
+
 def getCH4(mq2Reading):
-    return MQGetGasPercentage(MQResistanceCalculation(mq2Reading)/Ro,GAS_CH4)
+    return MQGetGasPercentage(MQResistanceCalculation(mq2Reading)/Ro, GAS_CH4)
+
 
 def getCO(mq2Reading):
-    return MQGetGasPercentage(MQResistanceCalculation(mq2Reading)/Ro,GAS_CO)
+    return MQGetGasPercentage(MQResistanceCalculation(mq2Reading)/Ro, GAS_CO)
+
 
 def getSmoke(mq2Reading):
-    return MQGetGasPercentage(MQResistanceCalculation(mq2Reading)/Ro,GAS_SMOKE)
+    return MQGetGasPercentage(MQResistanceCalculation(mq2Reading)/Ro, GAS_SMOKE)
+
 
 def getTemperature(bme680Reading):
     return bme680Reading.temperature
 
+
 def getPressure(bme680Reading):
     return bme680Reading.pressure
+
 
 def getHumidity(bme680Reading):
     return bme680Reading.humidity
 
+
 def getGasResistance(bme680Reading):
     return bme680Reading.gas_resistance
+
 
 def isVibrating(value):
     return not value
 
+
 def getIAQ(gasResistance, humidity):
     return math.log(gasResistance, 10) + 0.04*humidity
+
 
 def sampleSensorsData(MQ2Sensor, BME680Sensor):
     sumCH4 = 0
@@ -149,9 +158,9 @@ def sampleSensorsData(MQ2Sensor, BME680Sensor):
             sumHumidity = sumHumidity + getHumidity(bme680Reading)
             sumPressure = sumPressure + getPressure(bme680Reading)
             if bme680Reading.heat_stable:
-                sumGasResistance = sumGasResistance + getGasResistance(bme680Reading)
+                sumGasResistance = sumGasResistance + \
+                    getGasResistance(bme680Reading)
         time.sleep(0.1)
-
 
     CH4 = sumCH4/10
     CO = sumCO/10
@@ -173,6 +182,7 @@ def sampleSensorsData(MQ2Sensor, BME680Sensor):
 
     return sensorsData
 
+
 class GroveSW420Sensor():
     def __init__(self, channel):
         self.channel = channel
@@ -181,53 +191,23 @@ class GroveSW420Sensor():
     def read(self):
         return self.sensor.read()
 
+
 class DigitalLedButton(object):
-    '''
-    Grove Red/Yellow/Blue Led Button class
-    all of them has a gpio button with low valid level of pressing,
-    and a gpio led with high valid level for lighting.
-    Args:
-        pin(int): the number of gpio/slot your grove device connected.
-    '''
     def __init__(self, pin, invert_pins=False):
         output_pin = pin
         input_pin = pin + 1
-        if (invert_pins):
+        if (invert_pins):  # handling UART inversion of Tx and Rx
             output_pin, input_pin = input_pin, output_pin
 
-        # High = light on
+        # High = LED is HIGH
         self.led = Factory.getOneLed("GPIO-HIGH", output_pin)
-        # Low = pressed
+        # Low = Button is pressed
         self.__btn = Factory.getButton("GPIO-LOW", input_pin)
         self.__on_event = None
         self.__btn.on_event(self, DigitalLedButton.__handle_event)
 
     @property
     def on_event(self):
-        '''
-        Property access with
-            callback -- a callable function/object,
-                        will be called when there is button event
-            callback prototype:
-                callback(index, code, time)
-            callback argument:
-                Args:
-                    index(int): button index, be in 0 to [button count - 1]
-                    code (int): bits combination of
-                              -  Button.EV_LEVEL_CHANGED
-                              -  Button.EV_SINGLE_CLICK
-                              -  Button.EV_DOUBLE_CLICK
-                              -  Button.EV_LONG_PRESS
-                    time(time): event generation time
-                Returns: none
-        Examples:
-            set
-            .. code-block:: python
-                obj.on_event = callback
-            get
-            .. code-block:: python
-                callobj = obj.on_event
-        '''
         return self.__on_event
 
     @on_event.setter
@@ -237,14 +217,11 @@ class DigitalLedButton(object):
         self.__on_event = callback
 
     def __handle_event(self, evt):
-        # print("event index:{} event:{} pressed:{}"
-        #       .format(evt['index'], evt['code'], evt['presesed']))
-        if callable(self.__on_event):
-            # the customized behavior
+        if callable(self.__on_event):  # case where it is customized
             self.__on_event(evt['index'], evt['code'], evt['time'])
             return
 
-        # the default behavior
+        # no customization so default settings
         self.led.brightness = self.led.MAX_BRIGHT
         event = evt['code']
         if event & Button.EV_SINGLE_CLICK:
@@ -256,6 +233,7 @@ class DigitalLedButton(object):
         elif event & Button.EV_LONG_PRESS:
             self.led.light(False)
             print("turn off LED")
+
 
 def get_path_from_action(action):
     if (action == "attention"):
@@ -275,8 +253,10 @@ def get_path_from_action(action):
     elif (action == "cover_yourself"):
         return "wav_files/" + LANGUAGE + "/cover_yourself.py"
 
+
 def play_sound(action):
     call(["aplay", get_path_from_action(action)])
+
 
 def warn_if_necessary(data):
     co = data["poisonous"]["co"]
@@ -301,35 +281,42 @@ def warn_if_necessary(data):
         play_sound("earthquake")
         play_sound("cover_yourself")
 
+
 def on_connect(client, userdata, flags, rc):
-    if rc==0:
+    if rc == 0:
         print("Connected OK")
-        client.connected_flag=True #set flag
+        client.connected_flag = True  # set flag
     else:
-        print("Bad connection Returned code=",rc)
-        client.bad_connection_flag=True
+        print("Bad connection Returned code=", rc)
+        client.bad_connection_flag = True
+
 
 def on_disconnect(client, userdata, rc):
-    print("Disconnecting reason "  + str(rc))
-    client.connected_flag=False
-    client.disconnect_flag=True
+    print("Disconnecting reason " + str(rc))
+    client.connected_flag = False
+    client.disconnect_flag = True
+
 
 def on_message(client, data, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
+
 def on_publish(client, data, mid):
-    print("mid: "+ str(mid))
+    print("mid: " + str(mid))
+
 
 def on_subscribe(client, data, mid, granted_qos):
-    print("Subscribed: "+ str(mid) + " " + str(granted_qos))
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
 
 def on_log(client, data, level, string):
     print("log: " + string)
 
+
 def main():
     MQ2Sensor = GroveGasSensorMQ2(ANALOG_PIN)
     BME680Sensor = GroveBME680Sensor()
-    SW420Sensor =  GroveSW420Sensor(DIGITAL_INPUT_PIN)
+    SW420Sensor = GroveSW420Sensor(DIGITAL_INPUT_PIN)
     redButton = DigitalLedButton(DIGITAL_OUTPUT_PIN)
     yellowButton = DigitalLedButton(PWM_PIN)
     blueButton = DigitalLedButton(UART_PIN, True)
@@ -340,9 +327,9 @@ def main():
     print("Sensing...")
 
     client = paho.Client(client_id=username, transport="websockets")
-    client.username_pw_set(username=username,password=password)
-    client.connected_flag=False
-    client.bad_connection_flag=False
+    client.username_pw_set(username=username, password=password)
+    client.connected_flag = False
+    client.bad_connection_flag = False
 
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
@@ -354,23 +341,22 @@ def main():
     # thread triggers callbacks based on received messages
     client.loop_start()
 
-    print("Connecting to broker",broker)
+    print("Connecting to broker", broker)
 
     try:
-        client.connect(broker,port)
+        client.connect(broker, port)
     except e:
         print("Connection failed", e)
-        exit(1) # Should quit or raise flag to quit or retry
+        exit(1)
 
-    while not client.connected_flag: # waiting for CONNACK
+    while not client.connected_flag:  # waiting for CONNACK message
         print("Waiting for CONNACK...")
         time.sleep(1)
 
-    if client.bad_connection_flag: # established a connection but it's not OK
+    if client.bad_connection_flag:  # established a connection but it's not OK
         client.loop_stop()
         sys.exit()
 
-    # TODO subscribe to user control topics or something
     while (True):
         sensorsData = sampleSensorsData(MQ2Sensor, BME680Sensor)
         IAQ = getIAQ(sensorsData["GasResistance"], sensorsData["Humidity"])
@@ -379,12 +365,13 @@ def main():
         print('CH4 value: {0} ppm'.format(sensorsData["CH4"]))
         print('CO value: {0} ppm'.format(sensorsData["CO"]))
         print('SMOKE value: {0} ppm'.format(sensorsData["Smoke"]))
-        print('Temperature value: {0:.2f} C'.format(sensorsData["Temperature"]))
+        print('Temperature value: {0:.2f} C'.format(
+            sensorsData["Temperature"]))
         print('Pressure value: {0:.2f} hPa'.format(sensorsData["Pressure"]))
         print('Humidity value: {0:.2f} %RH'.format(sensorsData["Humidity"]))
         print('Indoor Air Quality: {0} '.format(IAQ))
-        print ('Vibration: {0}'.format(isVibrating(sw420Reading)))
-        print ("#####################################")
+        print('Vibration: {0}'.format(isVibrating(sw420Reading)))
+        print("#####################################")
 
         # defining a params dict for the parameters to be sent to the API
         data = {
@@ -405,10 +392,9 @@ def main():
 
         warn_if_necessary(simplejson.loads(simplejson.dumps(data)))
 
-        ret = client.publish(topic,simplejson.dumps(data), 2)
+        ret = client.publish(topic, simplejson.dumps(data), 2)
         time.sleep(sleep_time)
 
 
 if __name__ == "__main__":
     main()
-
